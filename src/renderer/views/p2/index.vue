e8119b9e63751ad7ceda81ad77b359c6843af60a
wpy2019 <90168183+wpy2019@users.noreply.github.com>
2022-11-23 17:01:58 +0800

    1
    1

--- src/renderer/views/p2/index.vue
+++ src/renderer/views/p2/index.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
+    <div style="padding-left: 20px;">
         <div>
             <canvas id="qrcode" width="140" height="140"></canvas>
             <div
                 style="height: 8mm; width:34mm; line-height: 8mm; text-align: center;font-size: 13px;overflow: hidden;">
                 1000000001000000010000001</div>
         </div>
-        <table class="tag" cellspacing="0">
+        <div style="height:30px;line-height: 30px;font-size: 12px;">图五</div>
+        <!-- 图五 -->
+        
+        <table class="tag" border="1px solid #ccc" cellspacing="0">
             <tr>
                 <td rowspan="4" style="background-color:aqua;width:40mm;"></td>
                 <td class="tag-tit">名称</td>
@@ -19,13 +22,13 @@
             </tr>
             <tr>
                 <td class="tag-tit">规格型号</td>
-                <td>启天M428</td>
+                <td style="width: 24mm;">启天M428</td>
                 <td class="tag-tit">分箱号</td>
                 <td>1/3</td>
             </tr>
             <tr>
                 <td class="tag-tit">生产日期</td>
-                <td>2021年07月08号</td>
+                <td style="width: 24mm;">2021年07月08号</td>
                 <td class="tag-tit">计量单位</td>
                 <td>套</td>
             </tr>
@@ -35,7 +38,73 @@
                 <td colspan="3">108549783120220824101926H</td>
             </tr>
         </table>
+    
+        <div style="height:30px;line-height: 30px;font-size: 12px;">图八</div>
+        <!-- 图八 -->
+       
+        <table class="tag" border="1px solid #ccc" cellspacing="0">
+            <tr>
+                <td rowspan="4" style="background-color:aqua;width:40mm;"></td>
+                <td class="tag-tit">名称</td>
+                <td colspan="3">春秋常服</td>
+            </tr>
+            <tr>
+                <td class="tag-tit">供应商</td>
+                <td colspan="3">际华三五〇三服装有限公司</td>
+                
+            </tr>
+            <tr>
+                <td class="tag-tit">规格型号</td>
+                <td colspan="3">165/88</td>
+            </tr>
+            <tr>
+                <td class="tag-tit">计量单位</td>
+                <td class="tag-msg">件</td>
+                <td class="tag-tit">数量</td>
+                <td class="tag-msg">100</td>
+            </tr>
+            <tr>
+                <td style="width:40mm;">品种码:868749282</td>
+                <td class="tag-tit">生产日期</td>
+                <td colspan="3">2021年07月08日</td>
+            </tr>
+        </table>
+    
+        <div style="height:30px;line-height: 30px;font-size: 12px;">图九</div>
+        <!-- 图九 -->
+        <table class="tag" border="1px solid #ccc" cellspacing="0">
+            <tr>
+                <td rowspan="4" style="background-color:aqua;width:40mm;"></td>
+                <td class="tag-tit">名称</td>
+                <td colspan="3">台式计算机</td>
+            </tr>
+            <tr>
+                <td class="tag-tit">供应商</td>
+                <td colspan="3">联想集团有限公司</td>
+                
+            </tr>
+            <tr>
+                <td class="tag-tit">规格型号</td>
+                <td class="tag-msg">启天M428</td>
+                <td class="tag-tit">分箱号</td>
+                <td class="tag-msg">1/3</td>
+            </tr>
+            <tr>
+                <td class="tag-tit">计量单位</td>
+                <td class="tag-msg">套</td>
+                <td class="tag-tit">数量</td>
+                <td class="tag-msg">100</td>
+            </tr>
+            <tr>
+                <td style="width:40mm;">品种号:AC0541420</td>
+                <td>单件码</td>
+                <td colspan="3">108549783120220824101926H</td>
+            </tr>
+        </table>
+
+        <div style="height:30px;line-height: 30px;font-size: 12px;"></div>
     </div>
+    
 </template>
 <script>
 import UQRCode from 'uqrcodejs'
@@ -78,21 +147,32 @@ export default {
 <style scoped>
 * {
     box-sizing: border-box;
-    font-size: 12px;
+    font-size: 6pt;
+    font-family: '黑体';
 }
+/* .box{
+    width: 100mm;
+    height: 50mm;
+    background-color: rgb(203, 231, 241);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+} */
 .tag {
-     width: 100mm;
-     height: 50mm;
-     
+     width: 98mm;
+     height: 45mm;
+     background-color: #fff;
  }
 .tag tr,td {
     height: 9mm;
     text-align: center;
-    border: 1px solid #000000;
+    
 }
 .tag-tit{
     width: 10mm;
     height: 9mm;
 }
-
+.tag-msg{
+    width: 18mm;
+}
 </style>
